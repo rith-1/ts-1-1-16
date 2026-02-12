127f7915d6b6e94a8b6c2b3cc5c6b2e9d4ba2ef7
Varun Katuri <varunkaturi07@gmail.com>
2026-02-12 18:13:36 -0500

    finished fileHander.java class

--- src/main/java/fileRead/fileHandler.java
+++ src/main/java/fileRead/fileHandler.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
+import java.io.IOException;
 
 
 import java.util.Collections;
@@ -20,21 +21,70 @@ public class fileHandler {
     public List<String> getFiles() {
     //method to get files in data folder
 
-        List<String> fileNmaes = new ArrayList<>();
+        List<String> fileNames = new ArrayList<>();
         Path folderPath = Paths.get(filesFolder);
 
-        if (!(Files.exists(folderPath))) {
+        if (!(Files.exists(folderPath)) || !(Files.isDirectory(folderPath))) {
             throw new RuntimeException("folder not found");
         }//check if file exists
 
-        for(Path path : folderPath ){
-            if(Files.isRegularFile(path)){
-                fileNmaes.add(path.getFileName().toString());
-            }
-        }//add files to List.
+        try{
+            File folder = folderPath.toFile();
+            //convert path to folder
+            File[] files = folder.listFiles();
+            //store files in array.
 
-        return fileNmaes;
+            if (files != null) {//make sure files arent empty
+                for(File file : files ){
+                    if(file.isFile()){
+                        fileNames.add(file.getName());
+                    }
+                }//add files to List.
+            }
+        }
+        catch (Exception e) {
+            System.out.println("error reading data folder" + e.getMessage());
+        }
+        //trycatch block to catch any errors while getting files from data folder
+
+        return fileNames;
     }
 
+    public String readFile(String fileName) {
+        String fileContents = "";
+        File file = new File(filesFolder + File.separator + fileName);
+        //create file object for targetted file. get file path with File.seperator.
+
+        if (!file.exists()) {
+            throw new RuntimeException("file not found");
+        }//if file is missing throw error
+
+        try{
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            //objects for line by line reading
+
+            String line = "";
+            //string to store each line of the file
+
+
+            while((line = bufferedReader.readLine()) != null){
+                fileContents += line;
+                fileContents += "\n";
+            }//read each line in file per iteration and adds line to the string. go to new line with \n
+
+            bufferedReader.close();
+            //close file reader.
+
+
+        }
+        catch (IOException e){
+            System.out.println("error reading the file" + e.getMessage());
+        }
+        //use try-catch block to catch any errors while reading file so code doesn't just crash
+
+        return fileContents;
+        //return file as a string
+    }
 
 }
