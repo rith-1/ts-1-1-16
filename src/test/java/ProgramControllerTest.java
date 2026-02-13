deddda56b4793f8c17f6e576b677b179cd4a8eb5
rithikanandula <rithika.nandula@gmail.com>
2026-02-13 16:11:55 -0500

    program controller test updates

--- src/test/java/ProgramControllerTest.java
+++ src/test/java/ProgramControllerTest.java
@@ -1,13 +1,9 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
 class ProgramControllerTest {
 
     @Mock
@@ -15,7 +11,6 @@ class ProgramControllerTest {
 
     private ProgramController controller;
 
-    @BeforeEach
     void setUp() {
         controller = new ProgramController(fileHandler);
     }
